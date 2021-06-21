b4a77c806f638958aee440c5f3b8051dbef121a0
Marcelo PONTES <marcelopontes.tele@gmail.com>
2021-06-22 00:03:41 +0200

    Arquivos do Terraform

---
+++ ansible/ec2.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+  access_key = "AKIA3OUABFA3C3UUFX6A"
+  secret_key = "6KfzeD5WY8RkKbUKan1q9PPpSMwfcHhqvNGlSbDj"
+  region     = "us-east-2"
+}
+
+resource "aws_instance" "example11" {
+  ami           = "ami-0d563aeddd4be7fff"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.security_group_instances2.name}"]
+  key_name = aws_key_pair.generated_key.key_name
+}
+
+variable "key_name" {}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.key_name
+  public_key = tls_private_key.example.public_key_openssh
+
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.example.private_key_pem}' > ./myKey.pem"
+  }
+}
+
+resource "aws_security_group" "security_group_instances2" {
+  name = "security_group_instances2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5222
+    to_port     = 5222
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5322
+    to_port     = 5322
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
